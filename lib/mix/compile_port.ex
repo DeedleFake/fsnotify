5609a26ed5c8ecb42a26bd47f6c634a0d98f0422
DeedleFake <deedlefake@users.noreply.github.com>
2025-11-21 11:06:40 -0500

    add a way to override the path to the `go` tool

--- lib/mix/compile_port.ex
+++ lib/mix/compile_port.ex
@@ -3,7 +3,7 @@ defmodule Mix.Tasks.Compile.CompilePort do
 
   use Mix.Task.Compiler
 
-  @go_path System.find_executable("go")
+  @go_path get_in(Mix.Project.config()[:fsnotify][:go_path]) || System.find_executable("go")
   @output_path Path.join(:code.priv_dir(:fsnotify), "fsnotify")
 
   @impl true
